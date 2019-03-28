5206464c95c9276dab50cbe5f5e244454493706d
Fadil Karajic <fadil.karajic@gmail.com>
2019-03-28 01:10:24 -0500

    First push

---
+++ DotNetMovies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetMovies.Startup))]
+namespace DotNetMovies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
